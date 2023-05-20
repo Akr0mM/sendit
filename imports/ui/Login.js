6c30d644cf3caf2a72511e9f2fe85aea46e671fd
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2023-05-20 05:37:14 +0200

    better login form

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -5,23 +5,36 @@ import './Login.html';
 import './style/Login.css';
 
 Template.login.events({
-    'submit form': function (event, template) {
+    'submit form': function (event) {
         event.preventDefault();
-        const emailOrUsername = template.find('#emailOrUsername').value;
-        const password = template.find('#password').value;
+        const emailOrUsername = event.target.emailOrUsername;
+        const password = event.target.password;
+
+        const emailOrUsernameValue = emailOrUsername.value;
+        const passwordValue = password.value;
 
         let loginSelector;
-        if (emailOrUsername.includes('@')) {
+        if (emailOrUsernameValue.includes('@')) {
             // L'entrée contient un '@', il s'agit donc d'un email
-            loginSelector = { email: emailOrUsername };
+            loginSelector = { email: emailOrUsernameValue };
         } else {
             // L'entrée ne contient pas de '@', il s'agit donc d'un nom d'utilisateur
-            loginSelector = { username: emailOrUsername };
+            loginSelector = { username: emailOrUsernameValue };
         }
 
-        Meteor.loginWithPassword(loginSelector, password, function (error) {
+        Meteor.loginWithPassword(loginSelector, passwordValue, function (error) {
             if (error) {
-                console.log(error.reason);
+                if (error.reason === "User not found") {
+                    emailOrUsername.value = "";
+                    emailOrUsername.placeholder = "Email ou Nom d'utilisateur incorrect";
+                    emailOrUsername.classList.add('incorrect-input');
+                    emailOrUsername.addEventListener('input', () => { emailOrUsername.classList.remove('incorrect-input'); emailOrUsername.placeholder = "Email ou Nom d'utilisateur"; });
+                } else if (error.reason === "Incorrect password") {
+                    password.value = "";
+                    password.placeholder = "Mot de passe incorrect";
+                    password.classList.add('incorrect-input');
+                    password.addEventListener('input', () => { password.classList.remove('incorrect-input'); password.placeholder = "Mot de passe"; });
+                }
             } else {
                 // Redirection vers la page chat après la connexion réussie
                 FlowRouter.go('/chat');
