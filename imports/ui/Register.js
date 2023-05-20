1a495bb56ddb463b410018a92c3fec19ea118047
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2023-05-20 05:37:08 +0200

    better register form

--- imports/ui/Register.js
+++ imports/ui/Register.js
@@ -7,15 +7,31 @@ import './style/Register.css';
 Template.register.events({
     'submit .register-form': function (event) {
         event.preventDefault();
+        const username = event.target.username;
+        const email = event.target.email;
+        const password = event.target.password;
 
-        var username = event.target.username.value;
-        var email = event.target.email.value;
-        var password = event.target.password.value;
+        const usernameValue = username.value;
+        const emailValue = email.value;
+        const passwordValue = password.value;
 
-        Meteor.call('appCreateUser', email, username, password, (err, res) => {
-            console.log('createUser', err, res);
+        Meteor.call('appCreateUser', emailValue, usernameValue, passwordValue, (err) => {
+            if (err.error === "username-already-exists") {
+                username.value = "";
+                username.placeholder = "Nom d'utilisateur déjà pris !";
+                username.classList.add('incorrect-input');
+                username.addEventListener('input', () => { username.classList.remove('incorrect-input'); username.placeholder = "Nom d'utilisateur"; });
+            }
+
+            if (err.error === "email-already-exists") {
+                email.value = "";
+                email.placeholder = "Email déjà pris !";
+                email.classList.add('incorrect-input');
+                email.addEventListener('input', () => { email.classList.remove('incorrect-input'); email.placeholder = "Email"; });
+            }
+
             if (!err) {
-                Meteor.loginWithPassword(email, password, (err) => {
+                Meteor.loginWithPassword(emailValue, passwordValue, (err) => {
                     if (err) {
                         console.log('loginWithPassword', err);
                     } else {
