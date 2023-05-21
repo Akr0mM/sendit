82b10293a5e1438b3d483ba5221e3bb274dce721
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2023-05-21 17:16:47 +0200

    adding accountSettingsModal

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,30 +1,33 @@
-import { Template } from "meteor/templating";
-import { Threads } from "../api/messages/collections";
+// @ts-ignore
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Template } from 'meteor/templating';
+import { Threads } from '../api/messages/collections';
+import { ProfilePictures } from '../api/users/ProfilePictures';
 
-import "./App.html";
-import "./Chat.js";
-import "./Form.js";
-import "./Navbar.js";
+import './App.html';
+import './style/App.css';
+import './Chat';
+import './Form';
+import './Navbar';
 
-import './Login.js';
-import './Register.js';
+import './Login';
+import './Register';
 
-const IS_LOADING_STRING = "isLoading";
+const IS_LOADING_STRING = 'isLoading';
 
-window.addEventListener("load", () => {
-  Meteor.call("userOnline");
+window.addEventListener('load', () => {
+  Meteor.call('userOnline');
 });
 
-window.addEventListener("beforeunload", () => {
-  Meteor.call("userOffline");
+window.addEventListener('beforeunload', () => {
+  Meteor.call('userOffline');
 });
 
 Template.chatMessages.onCreated(function chatContainerOnCreated() {
   this.state = new ReactiveDict();
 
-  const handlerThreads = Meteor.subscribe("threads");
-  const handlerMessages = Meteor.subscribe("messages");
+  const handlerThreads = Meteor.subscribe('threads');
+  const handlerMessages = Meteor.subscribe('messages');
   Tracker.autorun(() => {
     this.state.set(IS_LOADING_STRING, !handlerThreads.ready());
     this.state.set(IS_LOADING_STRING, !handlerMessages.ready());
@@ -34,12 +37,12 @@ Template.chatMessages.onCreated(function chatContainerOnCreated() {
 Template.chatMessages.helpers({
   isLoading() {
     const instance = Template.instance();
-    return instance.state.get(IS_LOADING_STRING);
+    return instance?.state.get(IS_LOADING_STRING);
   },
 });
 
-Template.chat.onCreated(function () {
-  Accounts.onLogout(function () {
+Template.chat.onCreated(() => {
+  Accounts.onLogout(() => {
     FlowRouter.go('/login');
   });
 });
@@ -48,6 +51,16 @@ Template.chat.helpers({
   openChat() {
     return Meteor.user()?.profile?.currentThreadId;
   },
+
+  showModal() {
+    // document.querySelector('.chat').classList.add('blur');
+    // return true;
+
+    if (Session.get('showModal')) {
+      document.querySelector('.chat').classList.add('blur');
+      return true;
+    } else return false;
+  },
 });
 
 Template.chatOpen.helpers({
@@ -55,23 +68,122 @@ Template.chatOpen.helpers({
     let groupName;
     const currentThreadId = Meteor.user()?.profile?.currentThreadId;
     if (!currentThreadId) {
-      return ["none"];
+      return ['none'];
     }
     const currentThread = Threads.findOne({ _id: currentThreadId });
     if (!currentThread) {
-      return ["none"];
+      return ['none'];
     }
 
-    const usersId = currentThread.usersId;
+    const { usersId } = currentThread;
     if (usersId.length > 2) groupName = currentThread?.groupName;
-    else if (usersId.length == 1) {
-      console.log("solo conv ?");
-    } else
-      usersId.forEach((userId) => {
-        if (userId !== Meteor.userId())
+    else if (usersId.length === 1) {
+      console.log('solo conv ?');
+    } else {
+      usersId.forEach(userId => {
+        if (userId !== Meteor.userId()) {
           groupName = Meteor.users.findOne({ _id: userId })?.username;
+        }
       });
+    }
 
     return groupName;
+  },
+});
+
+Template.accountSettingsModal.events({
+  'click .close-modal'() {
+    document.querySelector('.chat').classList.remove('blur');
+    Session.set('showModal', false);
+  },
+});
+
+Template.accountSettingsModal.helpers({
+  currentProfilePicture() {
+    return Meteor.user()?.profile?.pictureId;
+  },
+
+  profilePicture() {
+    const profilePictureId = Meteor.user()?.profile?.pictureId;
+    const profilePicture = ProfilePictures.findOne({ _id: profilePictureId });
+    return profilePicture;
+  },
+
+  firstLetter() {
+    const user = Meteor.user();
+    if (user && user.username) {
+      return user.username.charAt(0);
+    }
+    return '';
+  },
+});
+
+Template.uploadForm.onCreated(function () {
+  this.currentUpload = new ReactiveVar(false);
+  this.confirmation = new ReactiveVar(false);
+});
+
+Template.uploadForm.helpers({
+  currentUpload() {
+    // @ts-ignore
+    return Template.instance()?.currentUpload.get();
+  },
+
+  confirmation() {
+    // @ts-ignore
+    return Template.instance()?.confirmation.get();
+  },
+});
+
+Template.uploadForm.events({
+  'change #fileInput'(event, templateInstance) {
+    event.preventDefault();
+    if (event.currentTarget.files && event.currentTarget.files[0]) {
+      const upload = ProfilePictures.insert(
+        {
+          file: event.currentTarget.files[0],
+          chunkSize: 'dynamic',
+        },
+        false,
+      );
+
+      upload.on('start', function () {
+        templateInstance.currentUpload.set(this);
+      });
+
+      upload.on('end', (error, fileObj) => {
+        if (error) {
+          alert(`Error during upload: ${error}`);
+        } else {
+          Session.set(
+            'previousProfilePicture',
+            Meteor.user().profile.pictureId,
+          );
+          Meteor.users.update(Meteor.userId(), {
+            $set: { 'profile.pictureId': fileObj._id },
+          });
+
+          templateInstance.confirmation.set(true);
+        }
+        templateInstance.currentUpload.set(false);
+      });
+
+      upload.start();
+    }
+  },
+
+  'click #close-icon'(event, templateInstance) {
+    event.preventDefault();
+
+    templateInstance.confirmation.set(false);
+    Meteor.users.update(Meteor.userId(), {
+      $set: { 'profile.pictureId': Session.get('previousProfilePicture') },
+    });
+  },
+
+  'click #done-icon'(event, templateInstance) {
+    event.preventDefault();
+
+    templateInstance.confirmation.set(false);
   },
 });
