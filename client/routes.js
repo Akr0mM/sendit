27c07931fc4b2b0cca224ed8659f336518cb7b59
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2023-05-21 17:15:33 +0200

    adding routes

--- client/routes.js
+++ client/routes.js
@@ -2,30 +2,33 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
-    triggersEnter: [function (context, redirect) {
-        if (!Meteor.userId()) {
-            redirect('/chat');
-        } else {
-            redirect('/chat');
-        }
-    }]
+  triggersEnter: [
+    function (context, redirect) {
+      if (!Meteor.userId()) {
+        redirect('/chat');
+      } else {
+        redirect('/chat');
+      }
+    },
+  ],
 });
 
 FlowRouter.route('/login', {
-    action: function () {
-        BlazeLayout.render('app', { mainTemplate: 'login' });
-    }
+  action() {
+    BlazeLayout.render('app', { mainTemplate: 'login' });
+  },
 });
 
 FlowRouter.route('/register', {
-    action: function () {
-        BlazeLayout.render('app', { mainTemplate: 'register' });
-    }
+  action() {
+    BlazeLayout.render('app', { mainTemplate: 'register' });
+  },
 });
 
 FlowRouter.route('/chat', {
-    action: function () {
-        if (!Meteor.userId()) return FlowRouter.redirect('/login');
-        BlazeLayout.render('app', { mainTemplate: 'chat' });
-    }
+  // eslint-disable-next-line consistent-return
+  action() {
+    if (!Meteor.userId()) return FlowRouter.redirect('/login');
+    BlazeLayout.render('app', { mainTemplate: 'chat' });
+  },
 });
